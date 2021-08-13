99cbec9991049178aa9b989038c4b269d58effc4
Josy George <joe.george.josy@gmail.com>
2021-08-13 07:16:05 -0400

    heroku deployment-big fixed

--- best_deals/settings.py
+++ best_deals/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+#e)rk2048b!690)oxkkbiuqzqp312kuglp-w2z)3czwkx6=h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
